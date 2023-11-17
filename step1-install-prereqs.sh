ecfe1c507f402ab3e4dac0243188fa639a159d1c
Albino Man <albinoman1@gmail.com>
2023-11-17 00:37:24 -0700

    debug #9

--- step1-install-prereqs.sh
+++ step1-install-prereqs.sh
@@ -47,7 +47,8 @@ yarn --version
 echo "== yarn change version"
 corepack enable
 #yarn set version '3.x.x'
-yarn set version '3.x'
+#yarn set version '3.x'
+yarn set version '3.6.0'
 
 echo "== yarn version after trying to change version"
 which yarn
