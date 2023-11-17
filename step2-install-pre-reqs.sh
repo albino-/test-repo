8b46b4d75338222b93041f000399ec951166afb7
Albino Man <albinoman1@gmail.com>
2023-11-16 22:12:39 -0700

    Add step2 to CI github actions.

---
+++ step2-install-pre-reqs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+
+if [[ "${EUID}" -ne 0 ]]; then
+	echo "This script should only be run by the root user.  Exiting."
+	exit 1
+fi
+
+if [[ ! -f "/etc/apt/apt.conf.d/99localpref" ]]; then
+	echo 'APT::Install-Recommends "0";' > /etc/apt/apt.conf.d/99localpref
+	echo 'APT::Install-Suggests "0";' >> /etc/apt/apt.conf.d/99localpref
+fi
+
+apt update
+apt --yes install python3 ca-certificates curl wget
+
+#install yarn
+npm install --global yarn
