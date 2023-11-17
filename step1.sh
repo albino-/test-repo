f8d8c7a599e26669550be4dbc2b87b7862799e27
Albino Man <albinoman1@gmail.com>
2023-11-16 22:07:24 -0700

    Fix step1.sh

--- step1.sh
+++ step1.sh
@@ -2,6 +2,11 @@
 
 NODE_MAJOR=20  #major version of node to install
 
+if [[ "${EUID}" -ne 0 ]]; then
+	echo "This script should only be run by the root user.  Exiting."
+	exit 1
+fi
+
 if [[ ! -f "/etc/apt/apt.conf.d/99localpref" ]]; then
 	echo 'APT::Install-Recommends "0";' > /etc/apt/apt.conf.d/99localpref
 	echo 'APT::Install-Suggests "0";' >> /etc/apt/apt.conf.d/99localpref
